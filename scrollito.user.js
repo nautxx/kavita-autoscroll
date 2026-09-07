8a1efd97a386c9c51276c2266c7112cc749d2f07
Jason Barnachea <jrbarnachea@gmail.com>
2026-09-07 15:04:14 -0700

    chore: bump version to 1.2.0

--- scrollito.user.js
+++ scrollito.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Scrollito: auto-scroll for Kavita
 // @namespace    https://github.com/nautxx/scrollito
-// @version      1.1.0
+// @version      1.2.0
 // @description  Adjustable, pausable auto-scrolling for Kavita's Webtoon reader.
 // @author       nautxx
 // @license      MIT
@@ -18,7 +18,7 @@
 (() => {
   'use strict';
 
-  const VERSION = '1.1.0';
+  const VERSION = '1.2.0';
   const INSTALL_MARKER = 'data-scrollito';
   const STORAGE_KEY = 'scrollito.speed';
   const POSITION_STORAGE_KEY = 'scrollito.position';
